5a23cec9f5db0577f09e909c098e3984207d6d53
AndreyGorban <Whiteman1989@gmail.com>
2020-11-02 00:31:09 +0200

    Added ConsoleOutputHelpers class

---
+++ Shop_PPZ-31/views/helpers/ConsoleOutputHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shop_PPZ_31.models.viewModels;
+
+namespace Shop_PPZ_31.views.helpers
+{
+    static class ConsoleOutputHelpers
+    {
+        public static void OutputSimpleOrderView(SimpleOrderView o)
+        {
+            Console.WriteLine($"order id: {o.OrderV.Id}, customerId: {o.OrderV.CustomerId}, sum: {o.Sum}");
+        }
+
+        public static void OutputSimpleCustomerView(SimpleCustumerView c)
+        {
+            // TODO!!!!!!!!
+        }
+    }
+
+
+}
