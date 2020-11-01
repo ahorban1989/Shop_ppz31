f75c585587723b1de50ccbe2a913d3835b8c2bca
AndreyGorban <Whiteman1989@gmail.com>
2020-11-01 19:34:58 +0200

    Dddet helpers

--- Shop_PPZ-31/Controllers/CustomerManager.cs
+++ Shop_PPZ-31/Controllers/CustomerManager.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Shop_PPZ_31.models;
+using Shop_PPZ_31.models.viewModels;
+using System.Linq;
 
 namespace Shop_PPZ_31.Controllers
 {
-    class CustomerManager
+    static class CustomerManager
     {
+        static DBItem<Employee> dbEmployees = DBItem<Employee>.DBInstance();
+        static DBItem<Order> dbOrders = DBItem<Order>.DBInstance();
+        static DBItem<Customer> dbCustomers = DBItem<Customer>.DBInstance();
+        static DBItem<ProductOrder> dbProductOrders = DBItem<ProductOrder>.DBInstance();
+
+        #region CUSTOMER CRUD
+
+        #endregion
     }
 }
---
+++ Shop_PPZ-31/views/helpers/ImputHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop_PPZ_31.views.helpers
+{
+    static class ImputHelpers
+    {
+        public static string ImputName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
