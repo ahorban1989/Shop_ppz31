dc1135bd64612917433e832cf6a4d57c9464ebe2
Alexander Komarov <san4os2008@gmail.com>
2020-11-01 19:43:40 +0200

    Added 1-st level Menu Classes

---
+++ Shop_PPZ-31/views/CustomerManagerMenu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop_PPZ_31.views
+{
+    class CustomerManagerMenu : AbstractMenu
+    {
+        ConsoleColor conColorlor = ConsoleColor.Green;
+        ConsoleColor colorDefoult;
+
+        protected override void Idle()
+        {
+            Console.Clear();
+            Console.Title = "Customer Manager Menu";
+            //Console.WriteLine("\t\t\t\tMenu\n\nSelect menu: \n\n1. HR menu\n2. Product Manager Menu\n3. Costumer Manager Menu\n4. Exit");
+            Console.WriteLine("\t\t\t\tMenu\n\nSelect menu: \n\n1. Register Customer\n2. Delete Customer\n3. Create Order\n4. Delete Order\n5. Process Order\n6. Accept Payment\n7. Exit");
+            string switchMenu = Console.ReadLine();
+            // Таблица с Заказами. Просмотр Клиента. Детальнее о Заказе. Когда добавляем Заказ, показать табл. с Продуктами, и запрос ид Продукта, кол-ва, и ещё или закончить. Если Закончить, то введите ид Заказчика, или добавить нового.
+            // Далее выбираем Исполнителя Заказа. Enter, и Заказ ушёл в Таблицу.
+
+            switch (switchMenu)
+            {
+                case "1":
+                    //TODO Register Customer
+                    break;
+                case "2":
+                    //TODO Delete Customer
+                    break;
+                case "3":
+                    //TODO Create Order
+                    break;
+                case "4":
+                    //TODO Delete Order
+                    break;
+                case "5":
+                    //TODO Process Order
+                    break;
+                case "6":
+                    //TODO Accept Payment
+                    break;
+                case "7":
+                    //TODO Exit
+                    break;
+            }
+
+
+        }
+
+        protected override void Init()
+        {
+            colorDefoult = Console.ForegroundColor;
+
+            Console.ForegroundColor = conColorlor;
+        }
+
+        protected override void CleanUp()
+        {
+            Console.ForegroundColor = colorDefoult;
+        }
+    }
+}
---
+++ Shop_PPZ-31/views/ProductManagerMenu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop_PPZ_31.views
+{
+    class ProductManagerMenu : AbstractMenu
+    {
+        ConsoleColor conColorlor = ConsoleColor.Green;
+        ConsoleColor colorDefoult;
+
+        protected override void Idle()
+        {
+            Console.Clear();
+            Console.Title = "Product Manager Menu";
+            //Console.WriteLine("\t\t\t\tMenu\n\nSelect menu: \n\n1. HR menu\n2. Product Manager Menu\n3. Costumer Manager Menu\n4. Exit");
+            Console.WriteLine("\t\t\t\tMenu\n\nSelect menu: \n\n1. Register Product\n2. Delete Product\n3. Update Product\n4. Process Order\n5. Exit");
+            // Список всех Продуктов, а детальнее: 1)) Редактировать Продукт, 2)) Редактировать Описание, 3)) Удалить Продукт
+            string switchMenu = Console.ReadLine();
+
+            switch (switchMenu)
+            {
+                case "1":
+                    //TODO Register Product
+                    break;
+                case "2":
+                    //TODO Delete Product
+                    break;
+                case "3":
+                    //TODO Update Product
+                    break;
+                case "4":
+                    //TODO Process Order
+                    break;
+                case "5":
+                    //TODO Exit
+                    break;
+            }
+
+
+        }
+
+        protected override void Init()
+        {
+            colorDefoult = Console.ForegroundColor;
+
+            Console.ForegroundColor = conColorlor;
+        }
+
+        protected override void CleanUp()
+        {
+            Console.ForegroundColor = colorDefoult;
+        }
+    }
+}
