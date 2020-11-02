c8ebf61139a50ad4af5d0474a503fb89048a0ea0
AndreyGorban <Whiteman1989@gmail.com>
2020-11-02 19:51:44 +0200

    Finihed ConsoleOutputHelpers

--- Shop_PPZ-31/views/helpers/ConsoleOutputHelpers.cs
+++ Shop_PPZ-31/views/helpers/ConsoleOutputHelpers.cs
@@ -33,10 +33,51 @@ namespace Shop_PPZ_31.views.helpers
         public static void OutputProductOrderView(ProductOrderView po)
         {
             Console.WriteLine($"id: {po.ProuctOrderV.Id}, product: {po.ProductV.Name}" +
-                $", quantity: {po.ProuctOrderV.ProductCount}, price for 1: {po.ProuctOrderV.ProductPrice}" +
+                $", quantity: {po.ProuctOrderV.ProductCount}, price: {po.ProuctOrderV.ProductPrice}" +
                 $", sum: {po.ProuctOrderV.ProductCount*po.ProuctOrderV.ProductPrice}");
         }
 
+        public static void OutputOrderView(OrderView o)
+        {
+            Console.WriteLine($"id: {o.OrderV.Id}, customer id: {o.CustosumerV.Id}, customer name: {o.CustosumerV.Name}" +
+                $", employee name: {o.EmployeeV.Name}");
+
+            int sum = 0;
+            foreach (ProductOrderView po in o.ProductsOrderViewsV)
+            {
+                Console.Write("\t");
+                OutputProductOrderView(po);
+                //Console.WriteLine($"\tid: {po.ProuctOrderV.Id}, product name: {po.ProductV.Name}" +
+                //    $", quantity: {po.ProuctOrderV.ProductCount}, price: {po.ProuctOrderV.ProductPrice}" +
+                //    $", sum: {po.ProuctOrderV.ProductCount * po.ProuctOrderV.ProductPrice}");
+            }
+
+            Console.WriteLine($"total: {sum}");
+        }
+
+        public static void OutputEmployeeView (EmployeeView e)
+        {
+            Console.WriteLine($"id: {e.EmployeeV.Id}, name: {e.EmployeeV.Name}, surname: {e.EmployeeV.Surname}" +
+                $", position: {e.EmployeeV.Position} ,boss: {e.ChiefV.Name}");
+            Console.WriteLine("\t\tOrders:");
+            foreach (SimpleOrderView o in e.SimpleOrderViewsV)
+            {
+                Console.Write("\t\t");
+                OutputSimpleOrderView(o);
+            }
+        }
+
+
+        public static void OutputCusomerView(CustumerView c)
+        {
+            Console.WriteLine($"id: {c.CustomerV.Id}, name: {c.CustomerV.Name}, surname: {c.CustomerV.Surname}");
+            Console.WriteLine("\tOrdrs:");
+            foreach(SimpleOrderView o in c.SimpleOrderViewsV)
+            {
+                Console.Write("\t");
+                OutputSimpleOrderView(o);
+            }
+        }
 
     }
 
