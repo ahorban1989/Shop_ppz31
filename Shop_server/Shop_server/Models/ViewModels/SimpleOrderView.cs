a77d9f40a1b0aa8d15fac68becae28b0bc9bcf8b
AndreyGorban <Whiteman1989@gmail.com>
2021-01-31 01:07:46 +0200

    Added SimpleOrderViev Class

---
+++ Shop_server/Shop_server/Models/ViewModels/SimpleOrderView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop_server.Models.ViewModels
+{
+    public class SimpleOrderView
+    {
+        public int Id { get; set; }
+        public int ProductsCount { get; set; }
+        public decimal Sum { get; set; }
+
+    }
+}
