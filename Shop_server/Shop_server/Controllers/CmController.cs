729435015fd90e5238e88523e3adc83b6d0538f5
AndreyGorban <Whiteman1989@gmail.com>
2021-02-03 00:01:32 +0200

    added get functions to CmController class

--- Shop_server/Shop_server/Controllers/CmController.cs
+++ Shop_server/Shop_server/Controllers/CmController.cs
@@ -25,14 +25,66 @@ namespace Shop_server.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
         {
-            return await _context.Customers.ToListAsync();
+            return await _context.Customers.Include(c => c.Orders).Select(c =>
+                new Customer
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Surname = c.Surname,
+                    Orders = c.Orders
+                }
+            ).ToListAsync();
         }
 
         // GET: api/Cm/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Customer>> GetCustomer(int id)
         {
-            var customer = await _context.Customers.FindAsync(id);
+            var customer = await _context.Customers
+                    .Include(c => c.Orders)
+                    .ThenInclude(o => o.ProductOrders)
+                    .ThenInclude(po => po.Product)
+                    .Include(c => c.Orders)
+                    .ThenInclude(o => o.Employee)
+                    .Select(c =>
+                        new Customer
+                        {
+                            Id = c.Id,
+                            Name = c.Name,
+                            Surname = c.Surname,
+                            Orders = c.Orders.Select(o =>
+                                new Order
+                                {
+                                    Id = o.Id,
+                                    CustomerId = o.CustomerId,
+                                    EmployeeId = o.EmployeeId,
+                                    Customer = null,
+                                    Employee = new Employee
+                                    {
+                                        Id = o.Employee.Id,
+                                        Name = o.Employee.Name,
+                                        Surname = o.Employee.Surname,
+                                        Position = o.Employee.Position,
+                                        ChiefId = o.Employee.ChiefId,
+                                        Chief = null,
+                                        Orders = null
+                                    },
+                                    ProductOrders = o.ProductOrders.Select(po => 
+                                        new ProductOrder
+                                        {
+                                            Id = po.Id,
+                                            ProductId = po.ProductId,
+                                            ProductPrice = po.ProductPrice,
+                                            OrderId = po.OrderId,
+                                            ProductCount = po.ProductCount,
+                                            Product = po.Product
+                                        }
+                                    ).ToList()
+                                }
+                            ).ToList()
+                        }
+
+                    ).SingleOrDefaultAsync(c => c.Id == id);
 
             if (customer == null)
             {
